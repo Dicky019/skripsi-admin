b04d291bfffeaeb5a3d75c71abeda8156081bbb9
Dicky Darmawan <dicky93darmawan@gmail.com>
2023-08-02 22:35:02 +0800

    fix/ruteCreateSchema-regex-kode

--- src/types/rute.ts
+++ src/types/rute.ts
@@ -2,10 +2,11 @@ import { z } from "zod";
 import { locationSchema } from "./location";
 
 const regColor = /^#([0-9a-f]{3}|[0-9a-f]{6})$/i;
+const regKode = /[A-Z]/;
 
 export const ruteCreateSchema = z.object({
   name: z.string(),
-  kode: z.string().max(1).transform((v) => v.toUpperCase()),
+  kode: z.string().max(1).regex(regKode).transform((v) => v.toUpperCase()),
   color: z.string().regex(regColor),
   locationAwal: locationSchema,
   locationAkhir: locationSchema,
