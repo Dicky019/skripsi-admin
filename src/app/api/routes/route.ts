d17ba4ab948cdf26e1124764782db74e2b988e8b
Dicky Darmawan <dicky93darmawan@gmail.com>
2023-08-14 05:18:01 +0800

    .

--- src/app/api/routes/route.ts
+++ src/app/api/routes/route.ts
@@ -1,9 +1,9 @@
-import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 import { prisma } from "~/lib/db";
 
+export const revalidate = 0;
+
 export async function GET() {
-  revalidatePath("/api/routes")
   const data = await prisma.rute.findMany({
     include: {
       locations: true,
