4bf6e8e71a6be8f5973acaa6028659dddce62b39
Dicky Darmawan <dicky93darmawan@gmail.com>
2023-08-13 23:03:10 +0800

    .

--- src/app/api/rute/route.ts
+++ src/app/api/rute/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
-import { prisma } from "~/lib/db";
+import { getsRute } from "~/server/rute/gets";
 
 export async function GET() {
-  const data = await prisma.rute.findMany();
+  const data = await getsRute();
 
   return NextResponse.json({
     code: "200",
